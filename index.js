c5f7d0bf0d2525cd70958cd892d4a11ab5d27ea5
JMJ <josemichelle0331@gmail.com>
2026-07-29 20:51:54 -0400

    Add more places with their wifi information

--- index.js
+++ index.js
@@ -2,23 +2,80 @@
 const apartments = [
     {
         id: 1,
+        name: "Villa Santana - Hotel",
+        location: "Sajoma, Inoa",
+        img: "imgs/VSH.jpeg",
+        description: "None",
+        address: "Calle Padre Fortín 25, La Esmeralda, Santiago de los Caballeros, 51000, RD",
+        ssid: "BPN-((( Rafi ))) -5g",
+        pass: "3412085110",
+        maps: "https://www.google.com/maps/dir/?api=1&destination=19.350972,-70.988778"
+    },
+    {
+        id: 2,
         name: "B1 - Rialto Residences",
         location: "La Esmeralda, Santiago de los Caballeros",
         img: "imgs/B1R.avif",
         description: "None",
         address: "Calle Padre Fortín 25, La Esmeralda, Santiago de los Caballeros, 51000, RD",
         ssid: "Picasso08",
-        pass: "Picasso08"
+        pass: "Picasso08",
+        maps: "https://www.google.com/maps/dir/?api=1&destination=Rialto+Residences,+Calle+Padre+Fortín+25,+La+Esmeralda,+Santiago+de+los+Caballeros,+República+Dominicana"
     },
     {
-        id: 2,
+        id: 3,
         name: "B4 - Marcella Residences",
         location: "La Esmeralda, Santiago de los Caballeros",
         img: "imgs/B4M.webp",
         description: "None",
         address: "Calle Padre Fortín 25, La Esmeralda, Santiago de los Caballeros, 51000, RD",
         ssid: "Casso",
-        pass: "casso0804"
+        pass: "casso0804",
+        maps: "https://www.google.com/maps/dir/?api=1&destination=Rialto+Residences,+Calle+Padre+Fortín+25,+La+Esmeralda,+Santiago+de+los+Caballeros,+República+Dominicana"
+    },
+    {
+        id: 4,
+        name: "B4 - Jardines del Cerro II",
+        location: "Jardines del Cerro II, Santiago de los Caballeros",
+        img: "imgs/B4JDCII.avif",
+        description: "None",
+        address: "Jardines del Cerro II, Av. Hispanoamericana, Santiago de los Caballeros, 51000, RD",
+        ssid: "B4-5G",
+        pass: "B@420019",
+        maps: "https://www.google.com/maps/dir/?api=1&destination=Torres+Jardines+del+Cerro+II,+Av.+Hispanoamericana,+Santiago,+República+Dominicana"
+    },
+    {
+        id: 5,
+        name: "B4 - Soha Suites II",
+        location: "La Esmeralda, Santiago de los Caballeros",
+        img: "imgs/B4SSII.avif",
+        description: "None",
+        address: "Calle Padre Fortín 25, La Esmeralda, Santiago de los Caballeros, 51000, RD",
+        ssid: "SOHA 2 APTO B4",
+        pass: "Leev2023",
+        maps: "https://www.google.com/maps/dir/?api=1&destination=SOHA+Suites+II,+C.+Obispo+Morell+de+Sta.+Cruz+Esq,+Santiago+de+los+Caballeros+51000,+Dominican+Republic"
+    },
+    {
+        id: 6,
+        name: "A4 - Soha Suites II",
+        location: "La Esmeralda, Santiago de los Caballeros",
+        img: "imgs/A4SSII.avif",
+        description: "None",
+        address: "Calle Padre Fortín 25, La Esmeralda, Santiago de los Caballeros, 51000, RD",
+        ssid: "Apt4A_EXT",
+        pass: "8097345448",
+        maps: "https://www.google.com/maps/dir/?api=1&destination=SOHA+Suites+II,+C.+Obispo+Morell+de+Sta.+Cruz+Esq,+Santiago+de+los+Caballeros+51000,+Dominican+Republic"
+    },
+    {
+        id: 7,
+        name: "B9 - Rialto Residences",
+        location: "La Esmeralda, Santiago de los Caballeros",
+        img: "imgs/B9R.avif",
+        description: "None",
+        address: "Calle Padre Fortín 25, La Esmeralda, Santiago de los Caballeros, 51000, RD",
+        ssid: "Altice",
+        pass: "EJE6946e",
+        maps: "https://www.google.com/maps/dir/?api=1&destination=Rialto+Residences,+Calle+Padre+Fortín+25,+La+Esmeralda,+Santiago+de+los+Caballeros,+República+Dominicana"
     }
 ];
 
@@ -38,10 +95,10 @@ function renderApartments(filter = "") {
         card.className = "apt-card reveal active";
         card.innerHTML = `
                     <img src="${apt.img}" class="apt-img" loading="lazy">
-                    <div class="apt-info">
+                    <div class="apt-info" onclick="openModal(${apt.id})">
                         <h3 class="apt-name">${apt.name}</h3>
                         <p class="apt-loc">📍 ${apt.location}</p>
-                        <button class="btn-info" onclick="openModal(${apt.id})">Ver información</button>
+                        <button class="button button-empty button-sm">Ver información</button>
                     </div>
                 `;
         grid.appendChild(card);
@@ -74,7 +131,7 @@ window.openModal = function (id) {
     modalBody.innerHTML = `
                 <div class="modal-header">
                     <h2 style="font-size:1.5rem">${apt.name}</h2>
-                    <button class="close-modal" onclick="closeModal()">✕</button>
+                    <button class="button button-empty button-sm" onclick="closeModal()">✕</button>
                 </div>
                 <img src="${apt.img}" style="width:100%; border-radius:16px; margin-bottom:20px;">
                 <p style="color:#666">${apt.description}</p>
@@ -87,16 +144,19 @@ window.openModal = function (id) {
                     </div>
                     <div class="wifi-row">
                         <span>Clave: <strong id="pass-field">••••••••</strong></span>
-                        <button class="copy-btn" onclick="copyPass('${apt.pass}')">Copiar</button>
+                        <button class="button button-empty button-sm" onclick="copyPass('${apt.pass}')">Copiar</button>
                     </div>
-                    <button style="border:none; background:none; color:var(--primary); font-size:0.8rem; padding:0; cursor:pointer;" onclick="togglePass('${apt.pass}')">Mostrar contraseña</button>
+                    <button style="border:none; background:none; color:var(--primary); font-size:0.8rem; padding:0; cursor:pointer; text-decoration: underline;" onclick="togglePass('${apt.pass}')">Mostrar contraseña</button>
                 </div>
 
                 <div class="action-grid">
-                    <a href="https://maps.google.com/?q=${apt.name}" target="_blank" class="action-btn btn-maps">Ver en Maps</a>
-                    <a href="https://wa.me/1800000000" class="action-btn btn-wa">WhatsApp</a>
+                    <a href="${apt.maps}" target="_blank" 
+                    class="action-btn button button-empty button-sm">Ver en Maps</a>
+                    <a href="https://wa.me/18572597334" class="action-btn btn-wa button button-filled button-sm">WhatsApp</a>
                 </div>
             `;
+
+    // piensa en si es mejor usar el location's link del departamento en vez del nombre para abrir el mapa
     modalOverlay.classList.add('active');
     document.body.style.overflow = 'hidden';
 }
